6f2eaa8a2ec1ffe5a42e01a60aba7efaccc67553
mishra-avi-770 <144584334+mishra-avi-770@users.noreply.github.com>
2024-04-07 09:07:23 +0000

    datatypy

---
+++ BASICS/string.js
@@ -0,0 +1,24 @@
+//..................operation..................
+
+let value=3
+let negvalue= -value
+//console.log(negvalue)
+let power=2**3
+//console.log(power)
+
+//add string
+
+let s1= "Avinash"
+let s2= "Mishra"
+let s=s1+s2
+console.log(s);
+
+console.log(true)
+//output is true
+
+console.log(+true)
+//output is 1
+
+let num1,num2,num3
+
+
